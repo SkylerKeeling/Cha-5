5e933107f54c4e54f822abcf64d2a7e3bc998692
skyler <skyler@skylers-MacBook-Pro.local>
2023-07-06 16:44:26 -0300

    readme added

--- Assets/README.md
+++ Assets/README.md
@@ -1,1 +1,19 @@
 # Work Day Scheduler Starter Code
+
+## Overview
+
+Cha-6 is a workday planning tool.
+
+## Description
+
+Users can make notes for every working hour (9-5). The notes are saved to local storage and stored for the next time a user uses the application. It is also color coded to distinguish the time of day.
+
+## Screenshot
+
+(![Alt text](<images/Screen Shot 2023-07-06 at 4.41.26 PM.png>))
+
+## Application link
+
+## License
+
+Distributed under the MIT license.
--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,27 +1,10 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(function () {
-  // TODO: Add a listener for click events on the save button.
-
-  //This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage
-  // HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // const saveButton = document.querySelectorAll("#saveBtn")
-
-  //save to storage
   $(".saveBtn").on("click", function () {
     var textValue = $(this).siblings(".description").val()
     var hourKey = $(this).parent().attr("id")
     localStorage.setItem(hourKey, textValue)
   })
-  //print from storage
+
   $("#hour-9 .description").val(localStorage.getItem("hour-9"))
   $("#hour-10 .description").val(localStorage.getItem("hour-10"))
   $("#hour-11 .description").val(localStorage.getItem("hour-11"))
@@ -32,14 +15,6 @@ $(function () {
   $("#hour-4 .description").val(localStorage.getItem("hour-4"))
   $("#hour-5 .description").val(localStorage.getItem("hour-5"))
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  //
   function compareTime() {
     const currentTime = dayjs().format("H")
 
@@ -59,7 +34,6 @@ $(function () {
 
   compareTime()
 
-  //
   time = dayjs()
   $("#currentDay").text(time)
 })
